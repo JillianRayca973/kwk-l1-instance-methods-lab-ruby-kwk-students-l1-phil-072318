e1753db59386fd6316b6b8cd3cc1aa46787ba867
Jill  Rayca <rkrayca@yahoo.com>
2018-07-26 20:21:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # dog.rb
+
+class Dog
+  
+  def initialize(bark, sit)
+    @bark = bark
+    @sit = sit
+  end
+  
+  def return_bark
+    @bark
+    puts "Woof!"
+  end
+  
+  def return_sit
+    @sit
+    puts "The Dog is sitting"
+  end
+end
+
+susan= Dog.new("Woof!", "*sits*")
+susan.return_bark
+susan.return_sit
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # person.rb
+
+class Person
+  
+  def initialize(talk, walk)
+    @talk = talk
+    @walk = walk
+  end
+  
+  def return_talk
+    @talk
+    puts "Hello World!"
+  end
+  
+  def return_walk
+    @walk
+    puts "The Person is walking"
+  end
+end
+
+susan= Person.new("*walks*", "*sits*")
+
+susan.return_talk
+susan.return_walk
